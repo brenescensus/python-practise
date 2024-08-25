657f9baae3c4edc9fc92cc369853116ba242451c
brenescensus <76098751+brenescensus@users.noreply.github.com>
2024-08-25 21:35:34 +0300

    inputs with an area of a rectangle program

---
+++ python/inputs.py
@@ -0,0 +1,15 @@
+
+#this is how inputs are handled in python 
+ #specify the datatype of the input to get an accurate result
+# age=input("enter you age:")
+full_name=str(input("enter your full name:"))
+age=int(input("enter your age:"))
+print(f"you name is {full_name} and you are {age} years old")
+
+# print(f"your age is {age}")
+
+#area of a rectangle
+lenght=int(input("enter the lenght of the rectangle:"))
+width=int(input("enter the width of the rectangle:"))
+area=lenght*width
+print(f"the area of the rectangle is {area}")
