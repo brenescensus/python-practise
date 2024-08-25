fc35ed961cd46acc4a046d6e5a9e16da92fd9dcf
brenescensus <76098751+brenescensus@users.noreply.github.com>
2024-08-25 21:49:53 +0300

    shopping cart with  round function

---
+++ python/shoppingcart.py
@@ -0,0 +1,5 @@
+item=input("Enter the item:")
+price=float(input("Enter the price:"))
+quantity=int(input("Enter the quantity:"))
+total=price*quantity
+print(f"the total price for {item} is {round(total,2)}") 
